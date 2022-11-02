3f0f656e90caa1ddc7f5bd7e8ba0af373796db31
danas7046@gmail.com <123456Qwer>
2022-11-02 11:04:37 +0300

    Open-pages

---
+++ locators/elements_page.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+class TextBoxPageLocators:
+    #form fields
+
+    FULL_NAME = (By.CSS_SELECTOR,"input[id='userName']")
+    EMAIL =
+    CURRENR_ADDRESS =
+    PERMANENT_ADRESS =
+    SUBMIT =
+
+    #CREATED_FORM
+    CREATED_FULL_NAME =
+    CREATED_EMAIL =
+    CREATED_CURENT_ADDRESS =
+    CREATED_PERMANENT_ADDRESS =
+
