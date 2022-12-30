64facbed1a2ba44b609f109cd886710082a6cd5f
danas7046@gmail.com <123456Qwer>
2022-12-30 17:19:15 +0300

    Open-pages

--- tests/test.py
+++ tests/test.py
@@ -2,5 +2,5 @@ from  pages.base_page import  BasePage
 
 
 def  test(driver):
-    page = BasePage(driver, "https://github.com/Mirtalll/Avto-test-on-python")
+    page = BasePage(driver, "https://github.com")
     page.open()
